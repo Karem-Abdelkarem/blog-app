6560dbe3fa7332c988b55cdfc815b7f0772c37c1
Kareem Abdelkarem <km6263458@gmail.com>
2025-12-22 19:49:07 +0200

    add static data

---
+++ src/data/posts.js
@@ -0,0 +1,110 @@
+export const posts = [
+  {
+    id: 1,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-1.png",
+    author: {
+      name: "Tracy Wilson",
+      avatar: "/authors/author-1.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 2,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-2.png",
+    author: {
+      name: "Jason Francisco",
+      avatar: "/authors/author-2.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 3,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-3.png",
+    author: {
+      name: "Elizabeth Silvan",
+      avatar: "/authors/author-3.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 4,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-4.png",
+    author: {
+      name: "Erin Smith",
+      avatar: "/authors/author-4.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 5,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-5.png",
+    author: {
+      name: "Eric Smith",
+      avatar: "/authors/author-5.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 6,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-6.png",
+    author: {
+      name: "Tracy Wilson",
+      avatar: "/authors/author-6.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 7,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-7.png",
+    author: {
+      name: "Tracy Wilson",
+      avatar: "/authors/author-7.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 8,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-8.png",
+    author: {
+      name: "Tracy Wilson",
+      avatar: "/authors/author-8.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+  {
+    id: 9,
+    title:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    category: "Technology",
+    image: "/posts/post-9.png",
+    author: {
+      name: "Tracy Wilson",
+      avatar: "/authors/author-9.png",
+    },
+    publishedAt: "2022-08-20",
+  },
+];
